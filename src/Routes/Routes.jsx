4b225ad1a89686a153b61a9770da2c666f4307ad
Ñiura Gutierrez <niuragutierrez@gmail.com>
2025-04-25 20:37:45 -0600

    Mantenimiento-2

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,6 @@
 import Header from "../Components/Header/Header.jsx";
 import Home from "../Pages/Home/Home.jsx";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import NewVideo from "../Pages/NewVideo/NewVideo.jsx";
 import { VideoProvider } from "../Contexts/VideoContext.jsx";
 import Footer from "../Components/Footer/Footer.jsx";
@@ -9,18 +9,18 @@ import './AppRoutes.css';
 function AppRoutes() {
     return (
         <VideoProvider>
-            <BrowserRouter>
+            <HashRouter>
                 <div className="app-container">
                     <Header />
                     <div className="content">
                         <Routes>
-                            <Route index element={<Home />}></Route>
-                            <Route path="/newVideo" element={<NewVideo />}></Route>
+                            <Route index element={<Home />} />
+                            <Route path="/newVideo" element={<NewVideo />} />
                         </Routes>
                     </div>
                     <Footer />
                 </div>
-            </BrowserRouter>
+            </HashRouter>
         </VideoProvider>
     );
 }
--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
+  base: '/alura_flix/',  // Nombre exacto del repositorio
 });
